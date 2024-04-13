4f9141ee2af78382979de4cf764747d7ad433a6e
Borisresonance <llorentejuandiego@gmail.com>
2024-04-13 15:45:23 -0500

    added some page header and products page

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -8,11 +8,13 @@ import {
 import db from "@/db/db"
 import { formatCurrency, formatNumber } from "@/lib/formaters";
 
+//this will get sales data from DB
 async function getSalesData() {
     const data = await db.order.aggregate({
         _sum: { pricePaidInCents: true },
         _count: true
     })
+    await wait(2000)
 
     return {
         amount: (data._sum.pricePaidInCents || 0) / 100,
@@ -20,6 +22,11 @@ async function getSalesData() {
     }
 }
 
+function wait(duration: number) {
+    return new Promise(resolve => setTimeout(resolve, duration))
+}
+
+//this will get user data from DB
 async function getUserData() {
     const [userCount, orderData] = await Promise.all([
         db.user.count(),
@@ -28,10 +35,33 @@ async function getUserData() {
         })
     ])
 
+    return {
+        userCount,
+        averageValuePerUser:
+            userCount === 0
+                ? 0
+                : (orderData._sum.pricePaidInCents || 0) / userCount / 100,
+    }
+
+}
+//this will get Product data from Db
+async function getProductData() {
+    const [activeCount, inactiveCount] = await Promise.all([
+        db.product.count({ where: { isAvalibleForPurchase: true } }),
+        db.product.count({ where: { isAvalibleForPurchase: false } })
+    ])
+
+    return { activeCount, inactiveCount }
+
 }
 
+
 export default async function AdminDashboard() {
-    const salesData = await getSalesData()
+    const [salesData, userData, productData] = await Promise.all([
+        getSalesData(),
+        getUserData(),
+        getProductData()
+    ])
 
     return (
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
@@ -40,16 +70,28 @@ export default async function AdminDashboard() {
                 subtitle={`${formatNumber(salesData.numberOfSales)} Orders`}
                 body={formatCurrency(salesData.amount)}
             />
+
             <DashboardCard
                 title="Customers"
-                subtitle={`${formatCurrency(salesData.numberOfSales)} Average Value`}
-                body={formatNumber(salesData.amount)}
+                subtitle={`${formatCurrency(userData.averageValuePerUser
+                )} Average Value`}
+                body={formatNumber(userData.userCount)}
+            />
+            <DashboardCard
+                title="Active Products"
+                subtitle={`${formatNumber(productData.inactiveCount)} Inactive`}
+                body={formatNumber(productData.activeCount)}
             />
 
+
+
         </div>
+
     )
 
 }
+
+
 
 type DashboardCardProps = {
     title: string
@@ -59,18 +101,22 @@ type DashboardCardProps = {
 
 function DashboardCard({ title, subtitle, body }:
     DashboardCardProps) {
-    return
+    return (
+        <Card>
+            <CardHeader>
+                <CardTitle>{title}</CardTitle>
+                <CardDescription>{subtitle}</CardDescription>
+            </CardHeader>
 
-    <Card>
-        <CardHeader>
-            <CardTitle>{title}</CardTitle>
-            <CardDescription>{subtitle}</CardDescription>
-        </CardHeader>
+            <CardContent>
+                <p>{body}</p>
+            </CardContent>
 
-        <CardContent>
-            <p>{body}</p>
-        </CardContent>
 
-    </Card>
+
+        </Card>
+    )
+
+
 
 }

d4027775e47980d82ed318e7acea80bdb3dce82b
Borisresonance <llorentejuandiego@gmail.com>
2024-04-14 23:54:09 -0500

    changed the input route

--- src/components/ProductForm.tsx
+++ src/components/ProductForm.tsx
@@ -1,11 +1,13 @@
 "use client"
 
-import { Label } from "./ui/label"
+import { Label } from "@/components/ui/label"
+import { Input } from "@/components/ui/input"
 
 export function ProductForm() {
     return <form action="">
         <div>
             <Label></Label>
+            <Input type="text" id="name" name="name" requiered />
 
         </div>
     </form>
